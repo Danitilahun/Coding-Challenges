52c77095a484981b43eea363fc9f382863e130da
Daniel Tilahun <tiledan2015@gmail.com>
2024-12-18 20:24:39 +0300

    url databse model is added

---
+++ URL_Shortener/app/models/url.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String
+from app.db import Base
+
+class URL(Base):
+    __tablename__ = "urls"
+
+    id = Column(Integer, primary_key=True, index=True)
+    key = Column(String, unique=True, index=True)
+    short_url = Column(String, unique=True, index=True)
+    long_url = Column(String, nullable=False)
